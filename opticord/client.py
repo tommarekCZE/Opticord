4199e422ab3e41cedc196415dc0587b5ade4d04e
tommarekCZ-1 <80095089+tommarekCZE@users.noreply.github.com>
2023-02-15 15:50:26 +0100

    authentification 1.0

--- opticord/client.py
+++ opticord/client.py
@@ -1,4 +1,20 @@
 import aiosonic
+import asyncio
 
-def test():
-    return("If you seeing this dialog you are succefly tested Opticord")
+class authentificate:
+    def __init__(self, token):
+        self.token = token
+
+    def __str__(self):
+        loop = asyncio.get_event_loop()
+        client = str(loop.run_until_complete(self._authenticate(token=self.token)))
+        return client
+    async def _authenticate(self, token):
+        url = 'https://discord.com/api/users/@me'
+        headers = {
+            'Authorization': f'Bot {token}'
+        }
+
+        http_client = aiosonic.HTTPClient(verify_ssl=False)
+        response = await http_client.get(url=url, headers=headers, ssl=None)
+        return await response.json()
--- tests/test-functions.py
+++ tests/test-functions.py
@@ -1,7 +1,3 @@
 from opticord import client
-import asyncio
 
-def test_haversine():
-    print(client.test())
-
-test_haversine()
+print(client.authentificate(token="OTE5MzA1NjQ3MzA3MjQ3Njc3.Gx7LlQ.BY61BJ8KdwiBEMgRMrP_1TQfvKoSgHevKVACG8"))
