9c62462e023c5e242c00d357f33f1ffa98b2d9b7
tommarekCZ-1 <80095089+tommarekCZE@users.noreply.github.com>
2023-02-15 13:00:02 +0100

    body

---
+++ opticord/client.py
@@ -0,0 +1,4 @@
+import aiosonic
+
+def test():
+    return("If you seeing this dialog you are succefly tested Opticord")
---
+++ tests/test-functions.py
@@ -0,0 +1,7 @@
+from opticord import client
+import asyncio
+
+def test_haversine():
+    print(client.test())
+
+test_haversine()
